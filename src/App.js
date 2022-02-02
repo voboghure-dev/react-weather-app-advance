b796f2e825aba12273835fee9f733ab2919f73e8
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-02-02 16:45:01 +0600

    Add .env file with api data
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useState } from 'react';
+
 function App() {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const [noData, setNoData] = useState('No Data Yet');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [weatherData, setWeatherData] = useState([]);
+  const [city, setCity] = useState('Unknown Location');
+  const [weatherIcon, setWeatherIcon] = useState(
+    `${process.env.REACT_APP_ICON_URL}10n@2x.png`
+  );
+
   return (
-    <div className="app">
+    <div className='bg-gray-800 flex items-center justify-center w-screen h-screen py-10'>
       Welcome
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
